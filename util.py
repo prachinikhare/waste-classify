3ddd327383a32a3e50fa74be0f2d9f2dec9d8da8
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-01-21 00:45:50 +0530

    Update util.py

--- util.py
+++ util.py
@@ -36,7 +36,7 @@ data = {
 
 def load_artifacts():
     global model
-    model = tf.keras.models.load_model("classifyWaste.h5")
+    model = tf.keras.models.load_model("waste_classify.h5")
 
 def classify_waste(image_path):
 	global model, output_class
