b5dd3d2fd969c06f49dfc3942cd588fc8d2114e6
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-01-29 23:17:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,24 @@
  # save this as app.py
-from flask import Flask, escape, request, render_template
-import pickle
+from flask import Flask, flash, request, redirect, url_for, render_template
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 model = load_model('WASTE_CLASSIFY.h5')
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #about page
 @app.route("/")
@@ -14,19 +26,29 @@ def about():
     return render_template("about.html")
 
 #classify waste
-@app.route("/WASTE_CLASSIFY", methods=['GET', 'POST'])  
-def WASTE_CLASSIFY():
-    image_data = request.files["file"]
-    #save the image to upload
-    basepath = os.path.dirname(__file__)
-    image_path = os.path.join(basepath, "uploads", secure_filename(image_data.filename))
-    image_data.save(image_path)
-
-    predicted_value, details, video1, video2 = util.WASTE__CLASSIFY(image_path)
-    os.remove(image_path)
-    return jsonify(predicted_value=predicted_value, details=details, video1=video1, video2=video2)
-  else:
-    return render_template("classify.html")
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 # here is route of 404 means page not found error
 @app.errorhandler(404)
